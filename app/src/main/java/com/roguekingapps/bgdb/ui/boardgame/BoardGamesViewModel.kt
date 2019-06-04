6f234e3ec73ed07cd2d38086ed82a42bedd0e82b
Kenneth <kennethrogers.kr@gmail.com>
2019-06-04 12:27:01 +0100

    feature: Subscribe to observable in ViewModel to update LiveData.

--- app/src/main/java/com/roguekingapps/bgdb/ui/boardgame/BoardGamesViewModel.kt
+++ app/src/main/java/com/roguekingapps/bgdb/ui/boardgame/BoardGamesViewModel.kt
@@ -2,23 +2,32 @@ package com.roguekingapps.bgdb.ui.boardgame
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
+import com.roguekingapps.bgdb.data.database.BoardGame
 import com.roguekingapps.bgdb.data.network.BoardGamesRepository
 import com.roguekingapps.bgdb.data.network.Resource
-import com.roguekingapps.bgdb.data.network.Status
-import com.roguekingapps.bgdb.data.network.Status.LOADING
-import com.roguekingapps.bgdb.data.database.BoardGame
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
 
 class BoardGamesViewModel(private val boardGamesRepository: BoardGamesRepository) : ViewModel() {
 
-    private val _status = MutableLiveData<Status>()
+    private lateinit var subscriber : Disposable
 
-    val boardGames : LiveData<Resource<List<BoardGame>>> =
-        Transformations.switchMap(_status) { boardGamesRepository.getBoardGames() }
+    private val _boardGames  = MutableLiveData<Resource<List<BoardGame>>>()
+    val boardGames : LiveData<Resource<List<BoardGame>>>
+            get() = _boardGames
 
     fun getBoardGames() {
-        _status.value = LOADING
+        subscriber = boardGamesRepository.getBoardGames()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe { _boardGames.value = it }
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        subscriber.dispose()
     }
 
 }
