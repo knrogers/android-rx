6c71cf3970303bec1152265c0a4ff9cb3c0ae631
Kenneth <kennethrogers.kr@gmail.com>
2019-06-04 12:25:22 +0100

    feature: Return Maybe from dao.

--- app/src/main/java/com/roguekingapps/bgdb/data/database/BoardGameDao.kt
+++ app/src/main/java/com/roguekingapps/bgdb/data/database/BoardGameDao.kt
@@ -1,16 +1,16 @@
 package com.roguekingapps.bgdb.data.database
 
-import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy.REPLACE
 import androidx.room.Query
+import io.reactivex.Maybe
 
 @Dao
 interface BoardGameDao {
 
     @Query("SELECT * FROM boardgames")
-    fun getBoardGames() : LiveData<List<BoardGame>>
+    fun getBoardGames() : Maybe<List<BoardGame>>
 
     @Insert(onConflict = REPLACE)
     fun insertAll(boardGames: List<BoardGame>)
